260a6254be09a0dd4ad761b9b128110ba25416bb
cml <caimengli@realibox.com>
2021-01-17 01:18:07 +0800

    fix: 修改工程创建时文件缺失问题

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="yzcore",  # Replace with your own username
-    version="0.0.3",
+    version="0.0.7",
     author="cml",
     author_email="fu477521@163.com",
     description="An ID generator for distributed microservices",
@@ -20,6 +20,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/fu477521/yzcore.git",
     packages=setuptools.find_packages(),
+    package_data={'yzcore': [
+        'templates/project_template/*',
+        'templates/project_template/docs/*',
+        'templates/project_template/migrations/*',
+        'templates/project_template/src/*',
+        'templates/project_template/src/apps/*',
+        'templates/project_template/src/conf/*',
+        'templates/project_template/src/const/*',
+        'templates/project_template/src/tests/*',
+        'templates/project_template/src/utils/*',
+        'templates/project_template/.gitignore',
+    ]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -27,9 +39,9 @@ setuptools.setup(
     ],
     python_requires='>=3.6.8',
     install_requires=[
-        "fastapi>=0.61.1",
-        "sqlalchemy>=1.3.19",
-        "uvicorn>=0.11.8",
+        "fastapi>=0.63",
+        "sqlalchemy>1.3",
+        "uvicorn>0.13",
         "orjson>=3.3.1"
     ]
 )
