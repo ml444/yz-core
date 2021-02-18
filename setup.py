f73f3b40d835c0601a9ede7a95f38bf67f816043
mengli cai <caimengli@realibox.com>
2021-02-18 11:09:20 +0800

    fix: v0.0.15

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="yzcore",  # Replace with your own username
-    version="0.0.14",
+    version="0.0.15",
     author="cml",
     # author_email="caimengli@.com",
     description="An ID generator for distributed microservices",
